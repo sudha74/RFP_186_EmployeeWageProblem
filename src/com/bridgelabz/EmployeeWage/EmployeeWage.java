18e87a888d11dd9da652e0b26be056b9b641fce8
sudha123 <sudhanaukala1997@gmail.com>
2022-08-25 10:59:17 +0530

    MonthlyWage

--- src/com/bridgelabz/EmployeeWage/EmployeeWage.java
+++ src/com/bridgelabz/EmployeeWage/EmployeeWage.java
@@ -6,24 +6,30 @@ public class EmployeeWage {
     static final int WAGE_PER_HOUR = 20;
     static final int FULL_DAY_HOUR = 16;
     static final int PART_TIME_HOUR = 8;
+    static final int WORKING_DAY_PER_MONTH = 20;
+    static int monthlyWage = 0;
+    static int dailyWage = 0;
 
     public static void main(String[] args) {
         System.out.println("Welcome to Employee Wage");
-        int attendance = (int) (Math.floor(Math.random() * 10)) % 3;
-        switch (attendance) {
-            case IS_PRESENT:
-                System.out.println("Employee is Working Full Time");
-                int dailyWage = WAGE_PER_HOUR * FULL_DAY_HOUR;
-                System.out.println("Employee dailyWage is : " + dailyWage);
-                break;
-            case IS_PART_TIME:
-                System.out.println("Employee is Working Part Time");
-                dailyWage = WAGE_PER_HOUR * FULL_DAY_HOUR;
-                System.out.println("Employee dailyWage is : " + dailyWage);
-                break;
-            default:
-                System.out.println("Employee is absent");
-                break;
+        for (int day = 1; day < WORKING_DAY_PER_MONTH; day++) {
+            int attendance = (int) (Math.floor(Math.random() * 10)) % 3;
+            switch (attendance) {
+                case IS_PRESENT:
+//                System.out.println("Employee is Working Full Time");
+                    dailyWage = WAGE_PER_HOUR * FULL_DAY_HOUR;
+                    System.out.println("Employee dailyWage of day : " + day + " is " + dailyWage);
+                    break;
+                case IS_PART_TIME:
+                    dailyWage = WAGE_PER_HOUR * PART_TIME_HOUR;
+                    System.out.println("Employee dailyWage of day : " + day + " is " + dailyWage);
+                    break;
+                default:
+                    System.out.println("Employee is absent");
+                    break;
+            }
+            monthlyWage += dailyWage;
         }
+        System.out.println("MonthlyWage : " + monthlyWage);
     }
 }
